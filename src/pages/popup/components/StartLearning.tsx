5b095a25586bdb2c1ec2db9d77c97fdd4497ecff
Manmeet <mnmtsnghh@gmail.com>
2023-08-27 23:12:19 +0530

    add homepage

---
+++ src/pages/popup/components/StartLearning.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const StartLearning = () => {
+  return (
+    <>
+      <p className="font-bold text-5xl mb-5">
+        Your Personalized Learning Assistant.
+      </p>
+      <button className="border-2 border-solid border-black rounded-full mx-auto block px-5 py-2 bg-black text-white w-full">
+        Start Learning
+      </button>
+    </>
+  );
+};
+
+export default StartLearning;
