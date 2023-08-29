6785992acfcb130328cf1f68848400748a072917
Manmeet <mnmtsnghh@gmail.com>
2023-08-29 23:16:08 +0530

    add LessonPlan component

---
+++ src/pages/popup/components/LessonPlan.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+const LessonPlan = () => {
+  useEffect(() => {
+    // get preferences and messageId from chrome.local.storage
+  });
+  return <div>LessonPlan</div>;
+};
+
+export default LessonPlan;
